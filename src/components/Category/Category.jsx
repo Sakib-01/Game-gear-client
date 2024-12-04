2e78e0174ca3cc2c0e41e1c64e4129168969fd71
Sakib-01 <syedsakibulhaque2@gmail.com>
2024-12-04 20:40:05 +0600

    add react-awesome-reveal

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Fade, Zoom } from "react-awesome-reveal";
 
 const Category = () => {
   const [categories, setCategories] = useState([]);
@@ -15,25 +16,29 @@ const Category = () => {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
   return (
-    <div className="w-10/12 mx-auto  ">
-      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center  my-10 text-blue-500">
-        Categories{" "}
-        <span className="underline underline-offset-4 decoration-1 font-light text-green-600">
-          We Have
-        </span>
-      </h1>
+    <Fade cascade damping={0.1}>
+      <div className="w-10/12 mx-auto  ">
+        <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center  my-10 text-blue-500">
+          Categories{" "}
+          <span className="underline underline-offset-4 decoration-1 font-light text-green-600">
+            We Have
+          </span>
+        </h1>
 
-      <div className="grid grid-cols-3 md:grid-cols-5 gap-5 my-5">
-        {categories.map((category, index) => (
-          <div
-            className="px-2 py-2 md:px-10 md:py-5 rounded-3xl border-2 border-blue-400  text-center font-bold text-xl text-green-600"
-            key={index}
-          >
-            {category}
-          </div>
-        ))}
+        <div className="grid grid-cols-3 md:grid-cols-5 gap-5 my-5">
+          {categories.map((category, index) => (
+            <Zoom>
+              <div
+                className="px-2 py-2 md:px-10 md:py-5 rounded-3xl border-2 border-blue-400  text-center font-bold text-xl text-green-600"
+                key={index}
+              >
+                {category}
+              </div>
+            </Zoom>
+          ))}
+        </div>
       </div>
-    </div>
+    </Fade>
   );
 };
 
--- src/components/EquipmentCart/EquipmentCart.jsx
+++ src/components/EquipmentCart/EquipmentCart.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Fade, Zoom } from "react-awesome-reveal";
 import { Link } from "react-router-dom";
 
 const EquipmentCart = () => {
@@ -19,48 +20,57 @@ const EquipmentCart = () => {
         </span>
       </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center items-center gap-5 my-10 rounded-3xl  ">
-        {products.map((product) => (
-          <div
-            key={product._id}
-            className="card bg-base-100 shadow-xl  h-[550px]"
-          >
-            <figure>
-              <img
-                className="h-72 w-10/12 mx-auto rounded-full p-2"
-                src={product.image}
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body h-[200px]">
-              <h2 className="card-title">
-                {product.itemName}
-                <div className="flex items-center justify-between">
-                  <p
-                    className={`text-lg font-medium bg ${
-                      product.stockStatus > 0
-                        ? "text-green-600 bg-green-900 p-2 rounded-3xl"
-                        : "text-red-600 bg-red-900 p-2 rounded-3xl"
-                    }`}
-                  >
-                    {product.stockStatus > 0 ? "In Stock" : "Out of Stock"}
-                  </p>
+      <Fade cascade damping={0.1}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center items-center gap-5 my-10 rounded-3xl  ">
+          {products.map((product) => (
+            <div
+              key={product._id}
+              className="card bg-base-100 shadow-xl  h-[550px]"
+            >
+              <Zoom>
+                <figure>
+                  <img
+                    className="h-72 w-10/12 mx-auto rounded-full p-2"
+                    src={product.image}
+                    alt="Shoes"
+                  />
+                </figure>
+              </Zoom>
+              <div className="card-body h-[200px]">
+                <h2 className="card-title">
+                  {product.itemName}
+                  <div className="flex items-center justify-between">
+                    <p
+                      className={`text-lg font-medium bg ${
+                        product.stockStatus > 0
+                          ? "text-green-600 bg-green-900 p-2 rounded-3xl"
+                          : "text-red-600 bg-red-900 p-2 rounded-3xl"
+                      }`}
+                    >
+                      {product.stockStatus > 0 ? "In Stock" : "Out of Stock"}
+                    </p>
+                  </div>
+                </h2>
+                <div className="text-xl font-bold">
+                  {" "}
+                  Price: $ {product.price}
                 </div>
-              </h2>
-              <div className="text-xl font-bold"> Price: $ {product.price}</div>
-              <div className="card-actions justify-start gap-10">
-                <div className="text-xl font-bold">{product.categoryName} </div>
-                <div className="text-xl font-bold text-yellow-500">
-                  {product.rating} ★
+                <div className="card-actions justify-start gap-10">
+                  <div className="text-xl font-bold">
+                    {product.categoryName}{" "}
+                  </div>
+                  <div className="text-xl font-bold text-yellow-500">
+                    {product.rating} ★
+                  </div>
                 </div>
+                <Link to={`/equipmentdetails/${product._id}`}>
+                  <button className="btn btn-primary">Details</button>
+                </Link>
               </div>
-              <Link to={`/equipmentdetails/${product._id}`}>
-                <button className="btn btn-primary">Details</button>
-              </Link>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </Fade>
     </div>
   );
 };
